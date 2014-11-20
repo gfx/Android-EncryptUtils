13c83cdd8a4b02dc4aa5839e3ced89080adc196a
FUJI Goro (gfx) <gfuji@cpan.org>
2014-11-21 07:38:13 +0900

    tests

--- lib/src/androidTest/java/com/github/gfx/util/encrypt/LegacyEncryptionTest.java
+++ lib/src/androidTest/java/com/github/gfx/util/encrypt/LegacyEncryptionTest.java
@@ -1,49 +1,21 @@
 package com.github.gfx.util.encrypt;
 
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
-
 import android.content.ContentResolver;
 import android.content.Context;
 import android.test.AndroidTestCase;
 import android.test.mock.MockContext;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 
-@SuppressWarnings("Assert")
-public class EncryptionTest extends AndroidTestCase {
-
-    public void testDefaultPrivateKeyForContext() throws Exception {
-        final Context context = getContext();
-
-        byte[] k1 = Encryption.getDefaultPrivateKey(new MockContext() {
-            @Override
-            public String getPackageName() {
-                return "a";
-            }
-
-            @Override
-            public ContentResolver getContentResolver() {
-                return context.getContentResolver();
-            }
-        });
-        byte[] k2 = Encryption.getDefaultPrivateKey(new MockContext() {
-            @Override
-            public String getPackageName() {
-                return "b";
-            }
-
-            @Override
-            public ContentResolver getContentResolver() {
-                return context.getContentResolver();
-            }
-        });
-
-        assert !Arrays.equals(k1, k2);
-    }
+@SuppressWarnings({"Assert", "Deprecated"})
+public class LegacyEncryptionTest extends AndroidTestCase {
 
     public void testTooShortPrivateKey() throws Exception {
         try {
-            new Encryption(Encryption.getDefaultCipher(), "?");
+            new Encryption("?");
             fail();
         } catch (IllegalArgumentException e) {
             // OK
@@ -52,7 +24,7 @@ public class EncryptionTest extends AndroidTestCase {
 
     public void testTooLongPrivateKey() throws Exception {
         try {
-            new Encryption(Encryption.getDefaultCipher(), StringUtils.repeat(".", Encryption.KEY_LENGTH+1));
+            new Encryption(StringUtils.repeat(".", Encryption.KEY_LENGTH+1));
             fail();
         } catch (IllegalArgumentException e) {
             // OK
@@ -63,7 +35,7 @@ public class EncryptionTest extends AndroidTestCase {
     public void testEncryptDecrypt() throws Exception {
         for (int privateKeyPattern = 0; privateKeyPattern < 10; privateKeyPattern++) {
             String privateKey = RandomStringUtils.randomAscii(16);
-            Encryption encryption = new Encryption(Encryption.getDefaultCipher(), privateKey);
+            Encryption encryption = new Encryption(privateKey);
 
             for (int len = 1; len < 10000; len *= 2) {
                 for (int i = 0; i < 100; i++) {
@@ -79,7 +51,7 @@ public class EncryptionTest extends AndroidTestCase {
 
     public void testMultiByteString() throws Exception {
         String privateKey = RandomStringUtils.randomAscii(16);
-        Encryption encryption = new Encryption(Encryption.getDefaultCipher(), privateKey);
+        Encryption encryption = new Encryption(privateKey);
 
         String s = "日本語の混じった文字列。 Hello, world!";
         String encrypted = encryption.encrypt(s);
@@ -94,7 +66,7 @@ public class EncryptionTest extends AndroidTestCase {
     }
 
     public void testUsingDefaultPrivateKey() throws Exception {
-        Encryption encryption = new Encryption(Encryption.getDefaultCipher(), getContext());
+        Encryption encryption = new Encryption(getContext());
 
         String s = "Hello, world!";
         String encrypted = encryption.encrypt(s);
@@ -103,13 +75,13 @@ public class EncryptionTest extends AndroidTestCase {
         assert !s.equals(encrypted);
         assert decrypted.equals(s);
 
-        String decrypted2nd = new Encryption(Encryption.getDefaultCipher(), getContext()).decrypt(encrypted);
+        String decrypted2nd = new Encryption(getContext()).decrypt(encrypted);
 
         assert decrypted2nd.equals(decrypted);
     }
 
     public void testBadEncryption() throws Exception {
-        Encryption encryption = new Encryption(Encryption.getDefaultCipher(), getContext());
+        Encryption encryption = new Encryption(getContext());
 
         try {
             encryption.decrypt("foo");
